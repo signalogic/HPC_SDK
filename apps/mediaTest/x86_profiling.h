e3b0b3b9a718b1a7c51f8b770ddc3cf3acb7d988
Signalogic, Inc <info@signalogic.com>
2020-10-20 15:51:32 -0500

    Add files via upload

---
+++ apps/mediaTest/x86_profiling.h
@@ -0,0 +1,16 @@
+/*
+ $Header: /root/Signalogic/DirectCore/apps/SigC641x_C667x/mediaTest/x86_profiling.h
+
+ Copyright (C) Signalogic Inc. 2017
+ 
+ Description:
+ 
+   Header File for x86 mediaTest profiling
+
+ Revision History:
+ 
+   Created CJ, May 2017
+*/
+
+void init_x86_profiling();
+double getCurrentValue_cpuUsage();
